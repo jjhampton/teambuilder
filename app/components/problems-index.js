f30079aa5a2aae279560d3d409ed869b1e6ccc12
John Hampton <john_j_hampton@yahoo.com>
2015-07-07 17:30:45 -0400

    Move static data from component property to problems/index route model

--- app/components/problems-index.js
+++ app/components/problems-index.js
@@ -2,20 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'ul',
-  classNames: ['problems-index'],
-
-  problems: [
-    {
-      name: "Jones Family Home"
-    },
-    {
-      name: "Greenville Traffic Analysis"
-    },
-    {
-      name: "Middle School Math Tutoring"
-    },
-    {
-      name: "Community Garden Needs"
-    }
-  ]
+  classNames: ['problems-index']
 });
--- app/routes/problems/index.js
+++ app/routes/problems/index.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return [
+      {
+        name: "Jones Family Home"
+      },
+      {
+        name: "Greenville Traffic Analysis"
+      },
+      {
+        name: "Middle School Math Tutoring"
+      },
+      {
+        name: "Community Garden Needs"
+      }
+    ];
+  }
 });
