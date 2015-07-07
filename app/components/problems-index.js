cd5e47b1c5eb7d8590863a9fdb21eabfa5fa7980
John Hampton <john_j_hampton@yahoo.com>
2015-07-07 15:31:14 -0400

    Move data from hard-coded in template over to property in component Add each helper in component template

--- app/components/problems-index.js
+++ app/components/problems-index.js
@@ -2,5 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'ul',
-  classNames: ['problems-index']
+  classNames: ['problems-index'],
+
+  problems: [
+    {
+      name: "Jones Family Home"
+    },
+    {
+      name: "Greenville Traffic Analysis"
+    },
+    {
+      name: "Middle School Math Tutoring"
+    },
+    {
+      name: "Community Garden Needs"
+    }
+  ]
 });
