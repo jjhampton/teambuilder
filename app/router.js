3aed2c35a2163c1f310d9454fb678ee483262c83
John Hampton <john_j_hampton@yahoo.com>
2015-07-07 18:14:46 -0400

    Refactor route structure - designate index route and remove problems route

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('index', {path: '/'});
+
   this.route('sessions', function() {
     this.route('create');
   });
@@ -14,7 +16,6 @@ Router.map(function() {
     this.route('create');
     this.route('show');
   });
-  this.route('problems/index', {path: '/'});
 });
 
 export default Router;
