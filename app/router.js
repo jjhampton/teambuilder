c65a392713c2c764b303e6916e6dd0e723b68ebf
John Hampton <john_j_hampton@yahoo.com>
2015-07-07 15:12:21 -0400

    Move landing HTML to landing template, leaving only top nav in application template

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('landing');
+  this.route('landing', {path: '/'});
   this.route('sessions');
   this.route('users');
   this.route('problems');
