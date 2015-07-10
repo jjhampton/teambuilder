082acac4dcdf18a24292067b6c18ee7157a3b1ed
John Hampton <john_j_hampton@yahoo.com>
2015-07-10 12:32:51 -0400

    Add team member with volunteer button but not saving to Parse back-end

--- app/routes/problems/show.js
+++ app/routes/problems/show.js
@@ -23,11 +23,12 @@ export default Ember.Route.extend({
     //   });
     // }
     addUser: function(problem) {
-      console.log(problem);
-      console.log('clicked route');
       console.log(this.get('session.currentUser'));
+      console.log(problem);
+      console.log(problem.get('members'));
       var currentUser = this.get('session.currentUser');
-      problem.get('members').addObject(currentUser);
+      problem.get('members').addObject(this.get('session.currentUser'));
+      console.log(problem.get('members'));
       problem.save().then(function() {
         this.transitionTo('index');
       }.bind(this));
