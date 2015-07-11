3b673ef0b00f475c1a541c8066ba4f0772d64e71
John Hampton <john_j_hampton@yahoo.com>
2015-07-11 16:07:24 -0400

    Successfully remove user pointer field from Parse w/ REST API AJAX PUT in route

--- app/components/problems-manage.js
+++ app/components/problems-manage.js
@@ -2,10 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    remove: function(problem) {
-      console.log('remove clicked');
-      problem.set('member1', null);
-      problem.save();
+    removeMember: function(problem) {
+      console.log('component bubbled');
+      // console.log(problem.get('member1.username'));
+      // console.log('remove clicked');
+      // problem.set('member1.id', "undefined");
+      // problem.save();
+      this.sendAction('action', problem);
     }
   }
 });
--- app/routes/problems/manage.js
+++ app/routes/problems/manage.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    removeMember: function(problem) {
+      var adapter = this.store.adapterFor('problem');
+      console.log(adapter);
+      adapter.ajax("https://api.parse.com/1/classes/Problem/" + problem.id, 'PUT', {
+        data: {
+          'member1': {"__op":"Delete"}
+        }
+      }).then(function(response){
+        console.log(response);
+      });
+    }
+  }
+
 });
