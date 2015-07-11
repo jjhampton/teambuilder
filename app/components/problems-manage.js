3fa1b54d1c6852979959873ecc1d6decffcf502b
John Hampton <john_j_hampton@yahoo.com>
2015-07-10 23:39:27 -0400

    Successfully remove hard-coded model in component remove action handler

--- app/components/problems-manage.js
+++ app/components/problems-manage.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    remove: function(problem) {
+      console.log('remove clicked');
+      problem.set('member1', null);
+      problem.save();
+    }
+  }
 });
