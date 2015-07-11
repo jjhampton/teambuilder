78ffe3d8f92ad05331b9ba5a40a606a407e925e7
John Hampton <john_j_hampton@yahoo.com>
2015-07-11 19:37:31 -0400

    Console log value of thinker review input from within teammate-review component

--- app/components/teammate-review.js
+++ app/components/teammate-review.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    reviewTeammate: function() {
+      console.log('componenet > reviewTeammate');
+      var thinkerReview = this.get('thinkerReview');
+      console.log(thinkerReview);
+    }
+  }
 });
