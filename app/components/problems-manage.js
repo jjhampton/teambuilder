8397adc5ced2afcb9adf3c9678581aaad6a2ca11
John Hampton <john_j_hampton@yahoo.com>
2015-07-11 16:59:50 -0400

    Add removeMember action to all 5 member list items in problems/manage template Send string of member name as parameter which is encoded into AJAX data object as member to be deleted

--- app/components/problems-manage.js
+++ app/components/problems-manage.js
@@ -2,13 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    removeMember: function(problem) {
-      console.log('component bubbled');
-      // console.log(problem.get('member1.username'));
-      // console.log('remove clicked');
-      // problem.set('member1.id', "undefined");
-      // problem.save();
-      this.sendAction('action', problem);
+    removeMember: function(problem, member) {
+      this.sendAction('action', problem, member);
     }
   }
 });
--- app/routes/problems/manage.js
+++ app/routes/problems/manage.js
@@ -4,16 +4,18 @@ export default Ember.Route.extend({
   model: function() {
     return this.store.findAll('problem');
   },
+
   actions: {
-    removeMember: function(problem) {
+    removeMember: function(problem, member) {
       var adapter = this.store.adapterFor('problem');
+      var data = {};
+      data[member] = {"__op":"Delete"};
       console.log(adapter);
+      console.log('member in route is', member)
       adapter.ajax("https://api.parse.com/1/classes/Problem/" + problem.id, 'PUT', {
-        data: {
-          'member1': {"__op":"Delete"}
-        }
-      }).then(function(response){
-        problem.set('member1', null);
+        data: data
+      }).then(function(){
+        problem.set(member, null);
       });
     }
   }
