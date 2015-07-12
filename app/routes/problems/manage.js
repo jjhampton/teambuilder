a2d80c9245a01acd9ddd4b418739d019d6596fb2
John Hampton <john_j_hampton@yahoo.com>
2015-07-11 21:27:38 -0400

    Remove console.logs

--- app/routes/problems/manage.js
+++ app/routes/problems/manage.js
@@ -7,12 +7,9 @@ export default Ember.Route.extend({
 
   actions: {
     removeMember: function(problem, member) {
-      console.log('clicked route');
       var adapter = this.store.adapterFor('problem');
       var data = {};
       data[member] = {"__op":"Delete"};
-      console.log(adapter);
-      console.log('member in route is', member);
       adapter.ajax("https://api.parse.com/1/classes/Problem/" + problem.id, 'PUT', {
         data: data
       }).then(function(){
