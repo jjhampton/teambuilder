74b8fc735d62940e2ff65751bc863cb7719c9ae1
John Hampton <john_j_hampton@yahoo.com>
2015-07-12 02:58:12 -0400

    Add Gmaps import to brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -22,5 +22,6 @@ app.import('bower_components/pikaday/pikaday.js');
 app.import('bower_components/pikaday/css/pikaday.css');
 app.import('bower_components/underscore/underscore-min.js');
 app.import('bower_components/planetary.js/dist/planetaryjs.min.js');
+app.import('bower_components/gmaps/gmaps.min.js');
 
 module.exports = app.toTree();
