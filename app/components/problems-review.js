def6bbfa338b73d969d139be7319d876cadb99d8
John Hampton <john_j_hampton@yahoo.com>
2015-07-11 20:40:18 -0400

    Set up teammate-review component with action handler to submit review for first member - needs Cloud Code to let one user alter another user's properties

--- app/components/problems-review.js
+++ app/components/problems-review.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    reviewTeammate: function(user, thinkerReview, thinkerCurrent) {
+      console.log('component pr > reviewTeammate');
+      this.sendAction('action', user, thinkerReview, thinkerCurrent);
+    }
+  }
 });
--- app/components/teammate-review.js
+++ app/components/teammate-review.js
@@ -2,10 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    reviewTeammate: function() {
-      console.log('componenet > reviewTeammate');
-      var thinkerReview = this.get('thinkerReview');
+    reviewTeammate: function(user) {
+      console.log('component tr > reviewTeammate');
+      var thinkerReview = Number(this.get('thinkerReview'));
+      var thinkerCurrent = user.get('thinker');
       console.log(thinkerReview);
+      console.log(typeof thinkerReview);
+      console.log(thinkerCurrent);
+      this.sendAction('action', user, thinkerReview, thinkerCurrent);
     }
   }
 });
