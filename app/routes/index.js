186d28c66f2eae75a1699253d24d1c4e44652419
John Hampton <john_j_hampton@yahoo.com>
2015-07-15 11:19:17 -0400

    Create search results HTML element Add action handler on press of search button and log to console

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.findAll('problem');
+  },
+
+  actions: {
+    getResults: function() {
+      console.log('clicked');
+    }
   }
 });
