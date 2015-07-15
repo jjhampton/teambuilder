0fcabc2df3d46cbda18f33bd3ca35cefaa370074
John Hampton <john_j_hampton@yahoo.com>
2015-07-15 11:45:15 -0400

    Log value of search input form to console in action handler

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,6 +8,8 @@ export default Ember.Route.extend({
   actions: {
     getResults: function() {
       console.log('clicked');
+      var query = $('.index-search-input').val();
+      console.log('search input value', query);
       this.store.find('problem', 'u5UQU4E85T').then(function(response) {
         console.log(response.get('name'));
       });
