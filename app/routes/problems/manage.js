61f1af4cd239ceb442271830046175692810645b
John Hampton <john_j_hampton@yahoo.com>
2015-07-14 22:01:29 -0400

    Set all parse-user model attributes to {async:true}

--- app/routes/problems/manage.js
+++ app/routes/problems/manage.js
@@ -1,10 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    return this.store.findAll('problem');
-  },
-
+  
   actions: {
     removeMember: function(problem, member) {
       var adapter = this.store.adapterFor('problem');
