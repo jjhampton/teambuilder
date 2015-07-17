1463465334aa6cdf542aed0ec839c105ce875258
John Hampton <john_j_hampton@yahoo.com>
2015-07-17 01:30:11 -0400

    Add leaflet map to index and alter display options

--- app/components/leaflet-index.js
+++ app/components/leaflet-index.js
@@ -7,28 +7,26 @@ export default Ember.Component.extend({
       markerColor: 'red'
     });
 
-    var Stamen_TonerBackground = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png', {
+    var Stamen_Watercolor = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png', {
     	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     	subdomains: 'abcd',
-    	minZoom: 2,
-    	maxZoom: 10,
+    	minZoom: 1,
+    	maxZoom: 15,
     	ext: 'png'
     });
 
-    var map = L.map('map',{attributionControl: false, zoom: 5}).locate({setView: true});
-    var osmGeocoder = new L.Control.OSMGeocoder({
-            text: 'Enter location',
-			});
+    var map = L.map('indexmap', {attributionControl: false, zoomControl: false}).setView([40.866667, 10.566667], 1);
 
-    map.addControl(osmGeocoder);
-    Stamen_TonerBackground.addTo(map);
+    // map.fitWorld().zoomIn();
 
-    map.on('locationfound', onLocationFound);
+    Stamen_Watercolor.addTo(map);
 
-    function onLocationFound(e) {
-        // create a marker at the users "latlng" and add it to the map
-        L.marker(e.latlng, {icon: redMarker}, {draggable: true}).addTo(map);
-    }
+    // map.on('locationfound', onLocationFound);
+    //
+    // function onLocationFound(e) {
+    //     // create a marker at the users "latlng" and add it to the map
+    //     L.marker(e.latlng, {icon: redMarker}, {draggable: true}).addTo(map);
+    // }
 
 
 
