d15452b5bab368e61c97932db6ade711c29b4af0
John Hampton <john_j_hampton@yahoo.com>
2015-07-17 20:46:42 -0400

    Add computed property isMember to problems-show component and if-helper to only display team review button for members

--- app/components/problems-show.js
+++ app/components/problems-show.js
@@ -17,14 +17,16 @@ export default Ember.Component.extend({
 
   isOwner: function() {
     var problemOwnerId = this.get('model.owner.id');
-    console.log(problemOwnerId);
 
     var currentUserId = this.get('session.currentUser.id');
-    console.log(currentUserId);
     return (problemOwnerId === currentUserId);
+  }.property('model', 'session'),
+
+  isMember: function() {
+    var members = [this.get('model.owner.id'), this.get('model.member1.id'), this.get('model.member2.id'), this.get('model.member3.id'), this.get('model.member4.id'), this.get('model.member5.id')];
+    console.log(members);
+    var currentUserId = this.get('session.currentUser.id');
+    console.log(_.contains(members, currentUserId));
+    return _.contains(members, currentUserId);
   }.property('model', 'session')
 });
-//
-// latLng: function() {
-//   return [this.get('latitude'),  this.get('longitude')];
-// }.property('latitude', 'longitude')
