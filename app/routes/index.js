5e7b857846527a9e319b555f9a0340323fd22608
John Hampton <john_j_hampton@yahoo.com>
2015-07-20 22:06:42 -0400

    Add drop-shadow to maps, header, and primary-nav

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,15 @@ import Ember from 'ember';
 import ResetScroll from '../mixins/reset-scroll';
 
 export default Ember.Route.extend(ResetScroll, {
+
+  activate: function(){
+    $('.primary-nav').toggleClass("nav-shadow");
+  },
+
+  deactivate: function(){
+    $('.primary-nav').toggleClass("nav-shadow");
+  },
+
   model: function() {
     return this.store.findAll('problem');
   },
