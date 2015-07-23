104196fcdf45073fc54fe7f6f85b860a206bc920
John Hampton <john_j_hampton@yahoo.com>
2015-07-23 13:52:23 -0400

    Add CSS style to user search sort buttons

--- app/components/users-index.js
+++ app/components/users-index.js
@@ -3,22 +3,21 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'ul',
   classNames: ['users-index'],
+  sortProperties: ['totalPoints'],
+
+  actions: {
+    sortBy: function(property) {
+      this.set('sortProperties', [property]);
+    }
+  },
 
   sortedList: function() {
+    console.log(this.get('sortProperties'));
     return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
-      sortProperties: ['totalPoints'],
+      sortProperties: this.get('sortProperties'),
       sortAscending: false,
       content: this.get('model')
     });
-  }.property('model'),
+  }.property('model', 'sortProperties')
 
-  filteredList: function(){
-    // var filter = this.get('filter');
-    var users = this.sortedList;
-
-    return users.filter(function(user) {
-      return user.get('name').match(rx) || country.get('continent').match(rx);
-    });
-
-  }.property('arrangedContent', 'filter')
 });
