388466e0fde689bba6517ed56c21ea3980fc6ac6
John Hampton <john_j_hampton@yahoo.com>
2015-07-23 17:05:32 -0400

    Change CSS styling for user search sort buttons

--- app/components/problems-indexsearch.js
+++ app/components/problems-indexsearch.js
@@ -2,23 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'ul',
-  classNames: ['users-index'],
-  sortProperties: ['totalPoints'],
+  classNames: ['problems-index'],
+  sortProperties: ['progress'],
 
   actions: {
     sortBy: function(property) {
       $('.search-button-sort').removeClass('search-button-selected');
       $('button:hover').addClass('search-button-selected');
       this.set('sortProperties', [property]);
+    },
+
+    toggleOrder: function() {
+      this.get('sortedList').toggleProperty('sortAscending');
     }
   },
 
   sortedList: function() {
+    console.log(this.get('sortProperties'));
     return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
       sortProperties: this.get('sortProperties'),
-      sortAscending: false,
+      sortAscending: true,
       content: this.get('model')
     });
-  }.property('model', 'sortProperties')
-
+  }.property('model', 'sortProperties', 'sortAscending')
 });
