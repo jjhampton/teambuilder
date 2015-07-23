64f5aa52fd310b4a06598ed42bce33906f867cc0
John Hampton <john_j_hampton@yahoo.com>
2015-07-23 11:43:32 -0400

    Add Array proxy and SortableMixin to sort user search results descending by totalPoints

--- app/components/users-index.js
+++ app/components/users-index.js
@@ -2,5 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'ul',
-  classNames: ['users-index']
+  classNames: ['users-index'],
+
+  sortedList: function() {
+    return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
+      sortProperties: ['totalPoints'],
+      sortAscending: false,
+      content: this.get('model')
+    });
+  }.property('model'),
 });
